878756fb371b662bee4d332e25fa34541b267691
Sreerekha Deb <sree.deb@gmail.com>
2015-05-22 10:29:57 +0530

    Commit code to the git hub

---
+++ src/TestJUnit.java
@@ -0,0 +1,19 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * Learning Junit 
+ * @ date  21-May-2015
+ * @author Sreerekha 
+ */
+public class TestJUnit {
+
+	@Test
+	public void testAdd() {
+		//fail("Not yet implemented");
+		String str = "Junit is working fine **" ;
+		assertEquals("Junit is working fine",str);
+	}
+
+}
